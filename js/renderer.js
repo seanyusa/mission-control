7c3249735fd2e478114c55cb3b09510d7462b334
Sean <seanyusa@gmail.com>
2016-08-05 17:13:11 -0500

    Moved rendering to separate js function

---
+++ js/renderer.js
@@ -0,0 +1,25 @@
+function renderJson(jsonData)
+{
+	var teams = JSON.parse(jsonData);
+   	teamWidth = '19.5%';
+	var teamTemplate = document.getElementById('team-template').innerHTML;
+	Mustache.parse(teamTemplate);
+	var missionTemplate = document.getElementById('mission-template').innerHTML;
+	Mustache.parse(missionTemplate);
+
+	var rendered = "";
+
+	for (var teamNum in teams) {
+		var missionTileContent = "";
+		for (var i in teams[teamNum].missions) {
+			missionTileContent += Mustache.render(missionTemplate, teams[teamNum].missions[i]);
+		}
+		console.log(missionTileContent);
+		teams[teamNum].missionArea = missionTileContent;
+		teams[teamNum].teamWidth = teamWidth;
+
+		rendered += Mustache.render(teamTemplate, teams[teamNum]);
+	}
+
+	return rendered;
+}
